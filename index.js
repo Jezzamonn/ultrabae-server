bf307d6806578ba32e508cf78f13c9305c3d29ad
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-04-22 04:05:25 -0700

    Beginnings of something that can be used to relay messages

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+
+  socket.on('unity connect', function(username) {
+  	// Join a room with that username. I think that's all that's needed?
+  	socket.join(username);
+    console.log('unity joining room ' + username);
+  });
+  socket.on('phone connect', function(username) {
+  	// Ditto.
+  	socket.join(username);
+    console.log('phone joining room ' + username);
+  });
+
+  socket.on('set stat', function(msg){
+  	for (var room in socket.rooms) {
+  		io.to(room).emit('set stat', msg);
+  	}
+    console.log(socket.rooms);
   });
 });
 
