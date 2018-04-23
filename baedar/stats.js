4d5be52fb8d9db13848e5acac9a5c21643ecb2ca
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-04-22 17:48:16 -0700

    Add mising files

---
+++ baedar/stats.js
@@ -0,0 +1,86 @@
+window.options = [
+	{
+		name: "Karly, 22",
+		text: "Kind of slow, I respond faster on insta.",
+		stats: {
+			MaxHealth: 3,
+			Speed: 3,
+			NumBullets: 3,
+			BulletSpread: 30,
+			BulletCooldown: 0.2,
+			BulletLength: 4
+		}
+	},
+	{
+		name: "Samantha, 27",
+		text: "I moved here after Bumble banned guns. I love guns, the faster the gun the better! pewpewpewpew",
+		stats: {
+			MaxHealth: 2,
+			Speed: 5,
+			NumBullets: 1,
+			BulletSpread: 30,
+			BulletCooldown: 0.1,
+			BulletLength: 10
+		}
+	},
+	{
+		name: "Victor, 27",
+		text: "What's up? ;)\n\n",
+		stats: {
+			MaxHealth: 2,
+			Speed: 5,
+			NumBullets: 3,
+			BulletSpread: 30,
+			BulletCooldown: 0.3,
+			BulletLength: 6
+		}
+	},
+	{
+		name: "Joanna, 22",
+		text: "I could eat 14 tacos, if I eat more than you don't hit me up",
+		stats: {
+			MaxHealth: 5,
+			Speed: 4.5,
+			NumBullets: 1,
+			BulletSpread: 30,
+			BulletCooldown: 0.2,
+			BulletLength: 6
+		}
+	},
+	{
+		name: "Ryan, 19",
+		text: "Ka-BLAM!",
+		stats: {
+			MaxHealth: 1,
+			Speed: 6,
+			NumBullets: 5,
+			BulletSpread: 60,
+			BulletCooldown: 0.5,
+			BulletLength: 6
+		}
+	},
+	{
+		name: "Steven, 19",
+		text: "Looking to make some new friends. Add me on Snapchat :)",
+		stats: {
+			MaxHealth: 4,
+			Speed: 4,
+			NumBullets: 20,
+			BulletSpread: 360,
+			BulletCooldown: 0.2,
+			BulletLength: 2
+		}
+	},
+	{
+		name: "Jess, 25",
+		text: "Beach gal 🏖\nIf you own a harley, that's a plus! I love going FAST!",
+		stats: {
+			MaxHealth: 2,
+			Speed: 8,
+			NumBullets: 1,
+			BulletSpread: 10,
+			BulletCooldown: 0.2,
+			BulletLength: 8
+		}
+	},
+]
